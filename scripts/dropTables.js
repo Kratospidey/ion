5c383376179c6820ec882c0137b6ff5b273808b9
Kratospidey <devtools@kratospidey.com>
2024-02-19 21:35:28 +0530

    FIX DROP SCRIPT

--- scripts/dropTables.js
+++ scripts/dropTables.js
@@ -2,29 +2,34 @@ const db = require("../models"); // Import your Sequelize models
 
 // Function to drop all tables considering dependencies
 async function dropAllTablesConsideringDependencies() {
-    try {
-        // Drop tables with dependencies first
-        if (db.ServerUser) {
-            await db.ServerUser.drop();
-            console.log("Table ServerUser dropped.");
-        }
+	try {
+		if (db.Message) {
+			await db.Message.drop();
+			console.log("Table Message dropped.");
+		}
 
-        // Drop the Server table next, as it has a foreign key dependency on User
-        if (db.Server) {
-            await db.Server.drop();
-            console.log("Table Server dropped.");
-        }
+		// Drop tables with dependencies first
+		if (db.ServerUser) {
+			await db.ServerUser.drop();
+			console.log("Table ServerUser dropped.");
+		}
 
-        // Finally, drop the User table
-        if (db.User) {
-            await db.User.drop();
-            console.log("Table User dropped.");
-        }
+		// Drop the Server table next, as it has a foreign key dependency on User
+		if (db.Server) {
+			await db.Server.drop();
+			console.log("Table Server dropped.");
+		}
 
-        console.log("All tables dropped successfully considering dependencies.");
-    } catch (error) {
-        console.error("Error dropping tables considering dependencies:", error);
-    }
+		// Finally, drop the User table
+		if (db.User) {
+			await db.User.drop();
+			console.log("Table User dropped.");
+		}
+
+		console.log("All tables dropped successfully considering dependencies.");
+	} catch (error) {
+		console.error("Error dropping tables considering dependencies:", error);
+	}
 }
 
 // Execute the drop script
